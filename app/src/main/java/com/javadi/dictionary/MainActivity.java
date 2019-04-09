d7ad0a2ca822524f290d925abf9a229b70473a1c
javadi92 <farhad.javadi1371@yahoo.com>
2019-04-09 16:44:00 +0430

    add custom actionbar

--- app/src/main/java/com/javadi/dictionary/MainActivity.java
+++ app/src/main/java/com/javadi/dictionary/MainActivity.java
@@ -2,12 +2,18 @@ package com.javadi.dictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.toobar);
+        setSupportActionBar(toolbar);
     }
 }
